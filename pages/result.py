03798d4f77026d92b1ecaebaa2af4ca1160b7cfc
AndyLPK247 <AndyLPK247@gmail.com>
2019-08-25 22:06:24 -0400

    added DuckDuckGoResultPage locators

--- pages/result.py
+++ pages/result.py
@@ -3,12 +3,27 @@ This module contains DuckDuckGoResultPage,
 the page object for the DuckDuckGo search result page.
 """
 
+from selenium.webdriver.common.by import By
+
 
 class DuckDuckGoResultPage:
   
+  # Locators
+
+  SEARCH_INPUT = (By.NAME, 'q')
+
+  @staticmethod
+  def PHRASE_RESULTS(phrase):
+    xpath = f"//div[@id='links']//*[contains(text(), '{phrase}')]"
+    return (By.XPATH, xpath)
+  
+  # Initializer
+
   def __init__(self, browser):
     self.browser = browser
 
+  # Interaction Methods
+  
   def result_count_for_phrase(self, phrase):
     # TODO
     return 0
