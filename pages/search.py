9e0d69d1b674d26fd15cc6ee2c5b00d9d2fdd809
AndyLPK247 <AndyLPK247@gmail.com>
2019-08-25 22:06:16 -0400

    added DuckDuckGoSearchPage locators

--- pages/search.py
+++ pages/search.py
@@ -3,12 +3,22 @@ This module contains DuckDuckGoSearchPage,
 the page object for the DuckDuckGo search page.
 """
 
+from selenium.webdriver.common.by import By
+
 
 class DuckDuckGoSearchPage:
+
+  # Locators
+
+  SEARCH_INPUT = (By.NAME, 'q')
+
+  # Initializer
 
   def __init__(self, browser):
     self.browser = browser
 
+  # Interaction Methods
+  
   def load(self):
     # TODO
     pass
