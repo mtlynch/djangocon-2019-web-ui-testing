aaa6f77e8ac7dff3dcdb5ae217c7299e1673951c
AndyLPK247 <AndyLPK247@gmail.com>
2019-08-26 22:47:39 -0400

    updated conftest to read config file

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,18 +2,42 @@
 This module contains shared fixtures.
 """
 
+import json
 import pytest
 import selenium.webdriver
 
 
 @pytest.fixture
-def browser():
+def config(scope='session'):
 
-  # Initialize the ChromeDriver instance
-  b = selenium.webdriver.Chrome()
+  # Read the file
+  with open('config.json') as config_file:
+    config = json.load(config_file)
+  
+  # Assert values are acceptable
+  assert config['browser'] in ['Firefox', 'Chrome', 'Headless Chrome']
+  assert isinstance(config['implicit_wait'], int)
+  assert config['implicit_wait'] > 0
+
+  # Return config so it can be used
+  return config
+
+
+@pytest.fixture
+def browser(config):
+
+  # Initialize the WebDriver instance
+  if config['browser'] == 'Firefox':
+    b = selenium.webdriver.Firefox()
+  elif config['browser'] == 'Chrome':
+    b = selenium.webdriver.Chrome()
+  else:
+    opts = selenium.webdriver.ChromeOptions()
+    opts.add_argument('headless')
+    b = selenium.webdriver.Chrome(options=opts)
 
   # Make its calls wait up to 10 seconds for elements to appear
-  b.implicitly_wait(10)
+  b.implicitly_wait(config['implicit_wait'])
 
   # Return the WebDriver instance for the setup
   yield b
