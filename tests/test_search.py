9a185572054ca7d96f6aed0f6d728aa8c6657c06
AndyLPK247 <AndyLPK247@gmail.com>
2019-08-27 22:30:59 -0400

    parametrized test_basic_duckduckgo_search

--- tests/test_search.py
+++ tests/test_search.py
@@ -2,28 +2,30 @@
 These tests cover DuckDuckGo searches.
 """
 
+import pytest
+
 from pages.result import DuckDuckGoResultPage
 from pages.search import DuckDuckGoSearchPage
 
 
-def test_basic_duckduckgo_search(browser):
+@pytest.mark.parametrize('phrase', ['panda', 'python', 'platypus'])
+def test_basic_duckduckgo_search(browser, phrase):
   search_page = DuckDuckGoSearchPage(browser)
   result_page = DuckDuckGoResultPage(browser)
-  PHRASE = "panda"
   
   # Given the DuckDuckGo home page is displayed
   search_page.load()
 
   # When the user searches for "panda"
-  search_page.search(PHRASE)
+  search_page.search(phrase)
 
   # Then the search result query is "panda"
-  assert PHRASE == result_page.search_input_value()
+  assert phrase == result_page.search_input_value()
   
   # And the search result links pertain to "panda"
-  assert result_page.result_count_for_phrase(PHRASE) > 0
+  assert result_page.result_count_for_phrase(phrase) > 0
 
   # And the search result title contains "panda"
   # (Putting this assertion last guarantees that the page title will be ready)
-  assert PHRASE in result_page.title()
+  assert phrase in result_page.title()
   
